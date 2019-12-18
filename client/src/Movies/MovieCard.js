a0a56b16e6d3d5de1d7167ce0b41e3aeb1e2937e
rodrigograca31 <rodrigograca1993@gmail.com>
2019-12-18 22:53:50 +0000

    Part 2 - Deleting A Movie

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import Axios from "axios";
+
 const MovieCard = props => {
 	const { title, director, metascore, stars } = props.movie;
 	let { id } = useParams();
+	let history = useHistory();
 
+	const deleteMovie = id => {
+		Axios.delete(`http://localhost:5000/api/movies/${id}`)
+			.then(response => {
+				console.log(response);
+				history.push("/");
+			})
+			.catch(response => {
+				console.log(response);
+			});
+	};
 	return (
 		<div className="movie-card">
 			<Link to={`/update-movie/${id}`}>✏️</Link>
+			<br />
+			<br />
+			<div onClick={e => deleteMovie(id)}>🗑️</div>
 			<h2>{title}</h2>
 			<div className="movie-director">
 				Director: <em>{director}</em>
